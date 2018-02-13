6cdf2fbedfc33c7e5b7f8e80f0e10c9f4401c352
Terafu <maxime-dufour@outlook.fr>
2018-02-13 10:28:20 +0100

    Add user controller and routes
    Add user controller and routes

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,21 @@
+import User from '../models/user.model';
+
+export function getUsers(req, res) {
+
+}
+
+export function getUser(req, res) {
+
+}
+
+export function addUser(req, res) {
+
+}
+
+export function setUser(req, res) {
+
+}
+
+export function deleteUser(req, res) {
+	
+}
