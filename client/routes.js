6799456940a8696da1495f0f7de43c0791b03878
Léo Donny <leo.donny2@gmail.com>
2018-02-17 10:29:41 +0100

    front-base : init routing system

--- client/routes.js
+++ client/routes.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import Base from './modules/base/Base';
+import About from './modules/base/pages/AboutPage/AboutPage';
+import FAQ from './modules/base/pages/FaqPage/FaqPage';
 
 // require.ensure polyfill for node
 if (typeof require.ensure !== 'function') {
@@ -24,20 +26,17 @@ if (process.env.NODE_ENV !== 'production') {
 // More info: http://blog.mxstbr.com/2016/01/react-apps-with-pages/
 export default (
   <Route path="/" component={Base}>
-    <IndexRoute
+     {/* Base Routing */}
+    <Route path="/about" component={About}/>
+    <Route path="/faq" component={FAQ}/>
+
+   {/* Sample Routing */}
+    <Route path="/posts"       
       getComponent={(nextState, cb) => {
         require.ensure([], require => {
           cb(null, require('./modules/posts/pages/PostListPage/PostListPage').default);
         });
-      }}
-    />
-    <Route
-      path="/posts/:slug-:cuid"
-      getComponent={(nextState, cb) => {
-        require.ensure([], require => {
-          cb(null, require('./modules/posts/pages/PostDetailPage/PostDetailPage').default);
-        });
-      }}
+      }} 
     />
   </Route>
 );
