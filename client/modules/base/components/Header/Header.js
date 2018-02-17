c7a1a33925af1c4942902d4df63f2c7e82d9984e
Léo Donny <leo.donny2@gmail.com>
2018-02-17 10:29:41 +0100

    front-base : init routing system

--- client/modules/base/components/Header/Header.js
+++ client/modules/base/components/Header/Header.js
@@ -1,45 +1,101 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 import { FormattedMessage } from 'react-intl';
 
 // Import Style
 import styles from './Header.css';
 
-export function Header(props, context) {
-  const languageNodes = props.intl.enabledLanguages.map(
-    lang => <li key={lang} onClick={() => props.switchLanguage(lang)} className={lang === props.intl.locale ? styles.selected : ''}>{lang}</li>
-  );
+// Material UI
+import { withStyles } from 'material-ui/styles';
 
-  return (
-    <div className={styles.header}>
-      <div className={styles['language-switcher']}>
-        <ul>
-          <li><FormattedMessage id="switchLanguage" /></li>
-          {languageNodes}
-        </ul>
-      </div>
-      <div className={styles.content}>
-        <h1 className={styles['site-title']}>
-          <Link to="/" ><FormattedMessage id="siteTitle" /></Link>
-        </h1>
-        {
-          context.router.isActive('/', true)
-            ? <a className={styles['add-post-button']} href="#" onClick={props.toggleAddPost}><FormattedMessage id="addPost" /></a>
-            : null
-        }
-      </div>
-    </div>
-  );
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { Route, IndexRoute } from 'react-router';
+
+
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+
+import FeedbackIcon from 'material-ui/svg-icons/action/feedback';
+import QuestionAnswerIcon from 'material-ui/svg-icons/action/question-answer';
+import ExpandMoreIcon from 'material-ui/svg-icons/image/dehaze';
+
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+
+export default class Header extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+      open: false
+    };
+  }
+
+  handleToggle = () => {
+    this.setState({
+      open: !this.state.open
+    });
+  };
+
+  handleClose = () => {
+    this.setState({
+      open: false
+    });
+  };
+
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div>
+          <AppBar
+            title = "ETC"
+            iconElementLeft = {
+              <IconButton>
+                <ExpandMoreIcon />
+              </IconButton>
+            }
+            onLeftIconButtonClick	= {this.handleToggle}
+            iconElementRight = {
+              <div>
+                <IconButton>
+                  <FeedbackIcon />
+                </IconButton>
+                <IconButton>
+                  <QuestionAnswerIcon />
+                </IconButton>
+              </div>
+            }
+            className = {styles.appBarStyle}
+          />
+
+          <Drawer open={this.state.open} docked={false} width={300} onRequestChange={(open) => this.setState({open})}>
+            <AppBar title="ETC" showMenuIconButton={false} className = {styles.appBarStyle} />
+            <MenuItem className = {styles.menuTopic} disabled={true} >Infos</MenuItem>
+            
+            <Link to="/about" onClick={() => this.handleClose()}>
+              <MenuItem leftIcon = {
+                <ExpandMoreIcon />
+              } className = {styles.menuItem}>About</MenuItem>
+            </Link>
+            <Link to="/faq" onClick={() => this.handleClose()}>
+              <MenuItem leftIcon = {
+                <ExpandMoreIcon />
+              } className = {styles.menuItem}>FAQ</MenuItem>
+            </Link>
+            
+            <MenuItem className = {styles.menuTopic} disabled={true} >Sample</MenuItem>
+            <Link to="/posts" onClick={() => this.handleClose()}>
+              <MenuItem leftIcon = {
+                <ExpandMoreIcon />
+              } className = {styles.menuItem}>Posts</MenuItem>
+            </Link>
+          </Drawer>
+        </div>
+      </ MuiThemeProvider>
+    );
+  }
 }
 
 Header.contextTypes = {
   router: React.PropTypes.object,
 };
-
-Header.propTypes = {
-  toggleAddPost: PropTypes.func.isRequired,
-  switchLanguage: PropTypes.func.isRequired,
-  intl: PropTypes.object.isRequired,
-};
-
-export default Header;
---
+++ client/modules/base/pages/AboutPage/AboutPage.js
@@ -0,0 +1,18 @@
+import React, { PropTypes } from 'react';
+import Helmet from 'react-helmet';
+// import { FormattedMessage } from 'react-intl';
+
+// Import Style
+// import styles from './AboutPage.css';
+
+
+export function AboutPage(props) {
+  return (
+    <div>
+        <Helmet title="About" />
+        AboutPage
+    </div>
+  );
+}
+
+export default AboutPage;
