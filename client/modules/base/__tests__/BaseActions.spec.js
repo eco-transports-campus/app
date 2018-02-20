bc37262c5c615a6b44f527e85c52fe5cb6b0a78a
Léo Donny <leo.donny2@gmail.com>
2018-02-20 14:27:00 +0100

    comment front tests because we don't want to run old tests

--- client/modules/base/__tests__/BaseActions.spec.js
+++ client/modules/base/__tests__/BaseActions.spec.js
@@ -1,5 +1,5 @@
-import test from 'ava';
-import { actionTest } from 'redux-ava';
-import { TOGGLE_ADD_POST, toggleAddPost } from '../BaseActions';
+// import test from 'ava';
+// import { actionTest } from 'redux-ava';
+// import { TOGGLE_ADD_POST, toggleAddPost } from '../BaseActions';
 
-test('should return the correct type for toggleAddPost', actionTest(toggleAddPost, null, { type: TOGGLE_ADD_POST }));
+// test('should return the correct type for toggleAddPost', actionTest(toggleAddPost, null, { type: TOGGLE_ADD_POST }));
