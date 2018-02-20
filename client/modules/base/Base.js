ca0a475e6cf67e9568eb9b81a2054df04bd02e2a
Léo Donny <leo.donny2@gmail.com>
2018-02-20 14:25:07 +0100

    clean code on Base and DrawerContent

--- client/modules/base/Base.js
+++ client/modules/base/Base.js
@@ -1,27 +1,15 @@
+// React
 import React, { Component, PropTypes } from 'react';
 
-// Import Style
-// import baseStyle from './BaseStyle';
-import { Route, IndexRoute } from 'react-router';
+// MaterialUI
 import classNames from 'classnames';
 import { withStyles } from 'material-ui/styles';
 
-// Import Components
-import Helmet from 'react-helmet';
-import DevTools from './components/DevTools';
-import DrawerContent from './components/DrawerContent/DrawerContent';
-
-import { Router, browserHistory } from 'react-router';
-
-// MaterialUI stuff
+// MaterialUI Components
 import Drawer from 'material-ui/Drawer';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
-import List from 'material-ui/List';
-import { MenuItem } from 'material-ui/Menu';
-import TextField from 'material-ui/TextField';
 import Typography from 'material-ui/Typography';
-import Divider from 'material-ui/Divider';
 import IconButton from 'material-ui/IconButton';
 import Hidden from 'material-ui/Hidden';
 
@@ -33,61 +21,17 @@ import ReportIcon from 'material-ui-icons/Report';
 import FeedbackIcon from 'material-ui-icons/Feedback';
 import ExpandMoreIcon from 'material-ui-icons/Dehaze';
 
-// Import Actions
+// Style
+import { styles } from './BaseStyle';
+
+// Components
+import Helmet from 'react-helmet';
+import DevTools from './components/DevTools';
+import DrawerContent from './components/DrawerContent/DrawerContent';
+
+// Actions
 // import { } from './BaseActions';
 
-const styles = theme => ({
-  root: {
-    width: '100%',
-    height: 430,
-    marginTop: theme.spacing.unit * 3,
-    zIndex: 1,
-    overflow: 'hidden',
-    
-  },
-  appFrame: {
-    position: 'relative',
-    display: 'flex',
-    width: '100%',
-  },
-  appBar: {
-    position: 'fixed',
-    marginLeft: 240,
-    width: '100%',
-    zIndex: 1500,
-  },
-  navIconHide: {
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
-    },
-  },
-  drawerPaper: {
-    width: 250,
-    [theme.breakpoints.up('md')]: {
-      width: 240,
-      position: 'fixed',
-      height: '100vh',
-    },
-  },
-  content: {
-    backgroundColor: theme.palette.background.default,
-    width: '100%',
-    padding: theme.spacing.unit * 3,
-    height: 'calc(100% - 56px)',
-    marginTop: 56,
-    [theme.breakpoints.up('sm')]: {
-      height: 'calc(100% - 64px)',
-      marginTop: 64,
-    },
-    [theme.breakpoints.up('md')]: {
-      paddingLeft: 240,
-    },
-  },
-  typo :{
-    flex: 1,
-    paddingLeft: 20,
-  },
-});
 
 export class Base extends Component {
   constructor(props) {
@@ -174,7 +118,6 @@ export class Base extends Component {
 
 Base.propTypes = {
   classes: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles, { withTheme: true })(Base);
+export default withStyles(styles)(Base);
--- client/modules/base/components/DevTools.js
+++ client/modules/base/components/DevTools.js
@@ -1,3 +1,4 @@
+// React
 import React from 'react';
 import { createDevTools } from 'redux-devtools';
 import LogMonitor from 'redux-devtools-log-monitor';
