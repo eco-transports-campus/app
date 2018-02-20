e07fe54e8ae9b95c3194811c27b69a01c39beb6a
Léo Donny <leo.donny2@gmail.com>
2018-02-20 01:54:05 +0100

    separate DrawerContent to avoid code duplication

---
+++ client/modules/base/components/DrawerContent/DrawerContentStyle.js
@@ -0,0 +1,14 @@
+export const styles = theme => ({
+	drawerHeader: theme.mixins.toolbar,
+	drawerPaper: {
+		width: 250,
+		[theme.breakpoints.up('md')]: {
+		width: 240,
+		position: 'fixed',
+		height: '100vh',
+		},
+	},
+	menuItems: {
+		textDecoration: 'none',
+	}
+});
