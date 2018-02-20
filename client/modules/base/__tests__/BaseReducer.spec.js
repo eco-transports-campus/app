3600d1ad700f84c5a311cff4e8bcb0a8f38c6c52
Léo Donny <leo.donny2@gmail.com>
2018-02-20 14:27:00 +0100

    comment front tests because we don't want to run old tests

--- client/modules/base/__tests__/BaseReducer.spec.js
+++ client/modules/base/__tests__/BaseReducer.spec.js
@@ -1,17 +1,17 @@
-import test from 'ava';
-import { reducerTest } from 'redux-ava';
-import baseReducer, { getShowAddPost } from '../BaseReducer';
-import { toggleAddPost } from '../BaseActions';
+// import test from 'ava';
+// import { reducerTest } from 'redux-ava';
+// import baseReducer, { getShowAddPost } from '../BaseReducer';
+// import { toggleAddPost } from '../BaseActions';
 
-test('action for TOGGLE_ADD_POST is working', reducerTest(
-  baseReducer,
-  { showAddPost: false },
-  toggleAddPost(),
-  { showAddPost: true },
-));
+// test('action for TOGGLE_ADD_POST is working', reducerTest(
+//   baseReducer,
+//   { showAddPost: false },
+//   toggleAddPost(),
+//   { showAddPost: true },
+// ));
 
-test('getShowAddPost selector', t => {
-  t.is(getShowAddPost({
-    base: { showAddPost: false },
-  }), false);
-});
+// test('getShowAddPost selector', t => {
+//   t.is(getShowAddPost({
+//     base: { showAddPost: false },
+//   }), false);
+// });
