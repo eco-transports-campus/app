1f1caac4c7b25241df56ca14fc5fa3c67db435d4
Anuswan91 <vivien.blum@free.fr>
2018-03-06 09:25:12 -0800

    back(user-controller): Built model user

---
+++ server/models/user.model.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: { type: 'String', required: true }
+});
+
+export default mongoose.model('User', userSchema);
