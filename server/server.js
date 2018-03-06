15fa27aee7431a9c301b3e0edf50248a26969463
Anuswan91 <vivien.blum@free.fr>
2018-03-06 09:33:52 -0800

    back(user-controller): Added routes users to server

--- server/server.js
+++ server/server.js
@@ -37,6 +37,7 @@ import Helmet from 'react-helmet';
 import routes from '../client/routes';
 import { fetchComponentData } from './util/fetchData';
 import posts from './routes/post.routes';
+import users from './routes/user.routes';
 import dummyData from '../config/mocks/dummyData';
 import serverConfig from './config';
 
@@ -60,6 +61,7 @@ app.use(bodyParser.json({ limit: '20mb' }));
 app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
 app.use(Express.static(path.resolve(__dirname, '../dist/client')));
 app.use('/api', posts);
+app.use('/api', users);
 
 // Render Initial HTML
 const renderFullPage = (html, initialState) => {
