fae9d58d2802fb3052b21004608b0e50c6678edb
Anuswan91 <vivien.blum@free.fr>
2018-03-06 09:38:22 -0800

    back(user-controller): Done add user

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from '../models/user.model';
+import sanitizeHtml from 'sanitize-html';
 
 export function getUsers(req, res) {
 	User.find().exec((err, users) => {
@@ -14,7 +15,21 @@ export function getUser(req, res) {
 }
 
 export function addUser(req, res) {
+	if (!req.body.user.name) {
+    res.status(403).end();
+  }
 
+  const newUser = new User(req.body.user);
+
+  // Let's sanitize inputs
+  newUser.name = sanitizeHtml(newUser.name);
+
+  newUser.save((err, saved) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.json({ user: saved });
+  });
 }
 
 export function setUser(req, res) {
