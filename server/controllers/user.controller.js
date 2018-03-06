e6e88395be3a0db14d9002a49c02b966d413b829
Anuswan91 <vivien.blum@free.fr>
2018-03-06 09:33:52 -0800

    back(user-controller): Added routes users to server

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,7 +1,12 @@
 import User from '../models/user.model';
 
 export function getUsers(req, res) {
-
+	User.find().exec((err, users) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.json({ users });
+  });
 }
 
 export function getUser(req, res) {
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: { type: 'String', required: true }
+  'name': {'type': 'String', 'required': true}
 });
 
 export default mongoose.model('User', userSchema);
